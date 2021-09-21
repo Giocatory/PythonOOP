fc509204e51b908adad08fcaac7a44fbb1ca9c70
Giocatory <nivolyas@gmail.com>
2021-09-21 15:23:21 +0800

    21.09.2021

---
+++ how_to_create_class.py
@@ -0,0 +1,27 @@
+class Counter:
+    def start_from(self, count=0):
+        self.counter = count
+    def increment(self):
+        self.counter += 1
+    def display(self):
+        print(f"Текущее значение счетчика = {self.counter}")
+    def reset(self):
+        self.start_from()
+
+
+c1 = Counter()
+c1.start_from()
+c1.increment()
+c1.display()
+c1.increment()
+c1.display()
+c1.reset()
+c1.display()
+
+c2 = Counter()
+c2.start_from(3)
+c2.display()
+c2.increment()
+c2.display()
+c2.increment()
+c2.display()
