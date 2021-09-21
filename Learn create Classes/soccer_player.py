f205c24b55655cff6a6c94582bb6c618c724472e
Giocatory <nivolyas@gmail.com>
2021-09-21 16:11:27 +0800

    21.09.2021

---
+++ Learn create Classes/soccer_player.py
@@ -0,0 +1,21 @@
+class SoccerPlayer:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.goals = 0
+        self.assists = 0
+    def score(self, how_goals=1):
+        self.goals += how_goals
+    def make_assist(self, how_assist=1):
+        self.assists += how_assist
+    def statistics(self):
+        print(f"{self.surname} {self.name} - голы: {self.goals}, передачи: {self.assists}")
+
+
+leo = SoccerPlayer('Leo', 'Messi')
+leo.score(700)
+leo.make_assist(500)
+leo.statistics() # выводит "Messi Leo - голы: 700, передачи: 500"
+kokorin = SoccerPlayer('Alex', 'Kokorin')
+kokorin.score()
+kokorin.statistics() # выводит "Kokorin Alex - голы: 1, передачи: 0"
