4606740d6b1d439dd8beadda3337974c2ccc2d0a
Giocatory <nivolyas@gmail.com>
2021-09-21 16:20:41 +0800

    21.09.2021

---
+++ Learn create Classes/which_stripe_from_zebra.py
@@ -0,0 +1,24 @@
+#  Создайте класс Zebra,
+#  внутри которого есть метод which_stripe,
+#  который поочередно печатает фразы
+#  "Полоска белая", "Полоска черная",
+#  начиная именно с фразы "Полоска белая"
+class Zebra:
+    def __init__(self):
+        self.count = 0
+    def which_stripe(self):
+        if self.count % 2 == 0:
+            print("Полоска белая")
+            self.count += 1
+        else:
+            print("Полоска черная")
+            self.count += 1
+
+
+z1 = Zebra()
+for _ in range(5):
+    z1.which_stripe()
+print()
+z2 = Zebra()
+for _ in range(4):
+    z2.which_stripe()
