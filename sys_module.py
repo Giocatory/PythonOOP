b4cfe6d5d7e4025c91bea27c86d70e47c40a0bbc
Giocatory <nivolyas@gmail.com>
2021-09-29 06:49:27 +0300

    29.09.2021

--- sys_module.py
+++ sys_module.py
@@ -1,4 +1,6 @@
 import sys
-for k,v in sys.__dict__.items():
-    if not callable(v):
-        print("%20s: %s" % (k, repr(v)))
+
+with open('sys_info.txt', 'w') as out:
+    for k,v in sys.__dict__.items():
+        if not callable(v):
+            print("%20s: %s" % (k, repr(v)), file=out)
