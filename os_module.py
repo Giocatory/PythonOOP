4f8e1d40ac84163d27df2d8e84346036b62da63c
Giocatory <nivolyas@gmail.com>
2021-09-29 06:59:29 +0300

    29.09.2021

--- os_module.py
+++ os_module.py
@@ -1,6 +1,6 @@
 import os
 
 with open('os_info.txt', 'w') as out:
-    for k,v in os.__dict__.items():
+    for k, v in os.__dict__.items():
         if not callable(v):
             print("%20s: %s" % (k, repr(v)), file=out)
--- sys_module.py
+++ sys_module.py
@@ -1,6 +1,6 @@
 import sys
 
 with open('sys_info.txt', 'w') as out:
-    for k,v in sys.__dict__.items():
+    for k, v in sys.__dict__.items():
         if not callable(v):
             print("%20s: %s" % (k, repr(v)), file=out)
