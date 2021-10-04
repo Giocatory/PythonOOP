206dbf68356c325cb16be7b1b8daa3abaf018c95
Giocatory <nivolyas@gmail.com>
2021-10-04 09:14:04 +0300

    03.10.2021

---
+++ Encapsulation Get Set Static/static method.py
@@ -0,0 +1,28 @@
+class Student:
+    _how_many_class_created = 0
+
+    def __init__(self, id_student):
+        self.id = id_student  # instance attribute
+        Student._how_many_class_created += 1
+
+    @staticmethod
+    def How_many_class_created():
+        print(Student._how_many_class_created)
+
+    @staticmethod
+    def create_student(id_student):
+        return Student.__init__(id_student)
+
+    @classmethod
+    def new_student(cls, id_student):
+        return cls(id_student)
+
+
+st1 = Student.new_student(12)
+st2 = Student.new_student(13)
+st3 = Student.new_student(55)
+st4 = Student.new_student(102)
+st5 = Student.new_student(103)
+
+Student.How_many_class_created()
+st1.How_many_class_created()
