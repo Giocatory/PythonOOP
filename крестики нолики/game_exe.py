942782b9fd1e551e368e455182cdc09667dd7ed7
Giocatory <nivolyas@gmail.com>
2021-10-06 11:00:00 +0300

    04.10.2021

--- крестики нолики/game_exe.py
+++ крестики нолики/game_exe.py
@@ -12,10 +12,9 @@ field.show_field(main_field)
 # Создание игрока
 player = Create_Players.Player(input('Введите Имя игрока: '))
 print(f"Создан {str(player)}\n")
+
+
 # Цикл игры до победы
-cells = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
-
-
 def who_win():
     if main_field[0][0] == main_field[1][1] == main_field[2][2] != '.':
         return True
@@ -48,12 +47,15 @@ while not who_win():
                 main_field[player_row][player_col] = 'X'
                 field.show_field(main_field)
                 if who_win():
-                    print('Player Win')
+                    print(f'### {player.name_player} WINNER ###')
                     break
                 else:
                     break
+        if who_win():
+            break
         # Ходы Компьютера
         print("\nХод Компьютера")
+        cells = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
         computer_choice = random.choice(cells)
         while True:
             if main_field[computer_choice[0]][computer_choice[1]] != '.':
@@ -65,9 +67,11 @@ while not who_win():
                 main_field[computer_choice[0]][computer_choice[1]] = 'O'
                 field.show_field(main_field)
                 if who_win():
-                    print('Computer Win')
+                    print('### COMPUTER WINNER ###')
                     break
                 else:
                     break
+        if who_win():
+            break
     except Exception as ex:
         print(f'Не верно что-то ввели ({ex})')
